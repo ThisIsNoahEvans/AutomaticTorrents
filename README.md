78e77fe222e6b70ef868c3f7632057f266ee975d
Noah Evans <noah@itsnoahevans.co.uk>
2022-08-31 13:13:45 +0100

    Certs & unzip for Dockerfile install; NordVPN config in Setup script

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # AutomaticTorrents
 [WIP] Automatically add &amp; download torrents over VPN, then rename &amp; move them
+
+Only NordVPN is supported for now. Custom OpenVPN configuration will be available soon.
--- setup.sh
+++ setup.sh
@@ -4,6 +4,12 @@
 # Configure Transmission and OpenVPN in the container.                         #
 ################################################################################
 
+# ENVIRONMENT VARIABLES -- these will be set in Docker Compose but overridden here for testing
+export NORD_SERVER_ID='uk2239'
+export NORD_USERNAME='--'
+export NORD_PASSWORD='--'
+
+echo "Configuring Transmission..."
 # Stop Transmission
 service transmission-daemon stop
 # Update the Transmission configuration file -- this will eventually be on GitHub when the repo is public
@@ -11,3 +17,14 @@ wget 'https://cdn.itsnoahevans.co.uk/content/automatictorrents/transmission-conf
 # Start Transmission
 service transmission-daemon start
 
+echo "Configuring NordVPN..."
+# Save the NordVPN configuration file
+printf "%s" "$NORD_USERNAME\n$NORD_PASSWORD" > "/nordvpn/userpass.txt"
+# Setup NordVPN
+cd /etc/openvpn
+wget https://downloads.nordcdn.com/configs/archives/servers/ovpn.zip
+unzip ovpn.zip
+rm ovpn.zip
+cd /etc/openvpn/ovpn_udp/
+openvpn `$NORD_SERVER_ID.nordvpn.com.udp.ovpn` --auth-user-pass /nordvpn/userpass.txt
+echo "Connected to NordVPN!"
