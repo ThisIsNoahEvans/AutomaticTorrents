30b5ab8a747b18a9b8c743b98b47f8630d53bf64
Noah Evans <noah@itsnoahevans.co.uk>
2022-08-31 14:16:19 +0100

    Remove network device stuff

--- setup.sh
+++ setup.sh
@@ -32,12 +32,12 @@ echo ":: CD to /etc/openvpn/ovpn_udp/ "
 cd /etc/openvpn/ovpn_udp/
 
 # Create network device for VPN
-mkdir -p /dev/net
-mknod /dev/net/tun c 10 200
-chmod 0666 /dev/net/tun
+#mkdir -p /dev/net
+#mknod /dev/net/tun c 10 200
+#chmod 0666 /dev/net/tun
 
 # Connect to NordVPN
-openvpn --config "$NORD_SERVER_ID.nordvpn.com.udp.ovpn" --auth-user-pass /nordvpn/userpass.txt --dev /dev/net/tun 
+openvpn --config "$NORD_SERVER_ID.nordvpn.com.udp.ovpn" --auth-user-pass /nordvpn/userpass.txt #--dev /dev/net/tun 
 echo "Connected to NordVPN!"
 
 sleep infinity
