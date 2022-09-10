1842db4cfef87488037195ba969235578f390e2c
Noah Evans <noah@itsnoahevans.co.uk>
2022-09-10 14:55:55 +0100

    Firefox in Docker file & sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 ################################################################################
 # Configure Transmission and OpenVPN in the container.                         #
